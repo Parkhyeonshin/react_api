b13c107037c6161a574c42aa7cefc4e0e0a8d9ee
Parkhyeonshin <sshin4882@gmail.com>
2022-11-08 20:42:47 +0900

    22

--- src/components/include/MovieList.jsx
+++ src/components/include/MovieList.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+import { EffectCoverflow, Pagination } from "swiper";
 
 function MoviePopular(props) {
     return (
-        <li>
-            <a href={`https://www.themoviedb.org/movie/${props.movie.id}`}>
-                <img src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} alt={props.movie.title} />
-                <em>
-                    <span className="title">{props.movie.title}</span>
-                    {/* <span className="star">{props.movie.vote_average}</span> */}
-                    <span className="star">{props.index + 1}위</span>
-                </em>
-            </a>
-        </li>
+        <a href={`https://www.themoviedb.org/movie/${props.movie.id}`}>
+            <img src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} alt={props.movie.title} />
+            <em>
+                <span className="title">{props.movie.title}</span>
+                {/* <span className="star">{props.movie.vote_average}</span> */}
+                <span className="star">{props.index + 1}위</span>
+            </em>
+        </a>
     );
 }
 
@@ -20,10 +23,32 @@ const MovieList = (props) => {
         <section className="cont__movie">
             <div className="container">
                 <div className="movie__pop">
+                    <div className="movie__popname">인기 순위</div>
                     <ul>
-                        {props.popular.map((popular, index) => (
-                            <MoviePopular key={index} movie={popular} index={index} />
-                        ))}
+                        <Swiper
+                            effect={"coverflow"}
+                            grabCursor={true}
+                            centeredSlides={true}
+                            slidesPerView={"auto"}
+                            coverflowEffect={{
+                                rotate: 50,
+                                stretch: 0,
+                                depth: 100,
+                                modifier: 1,
+                                slideShadows: true,
+                            }}
+                            pagination={true}
+                            modules={[EffectCoverflow, Pagination]}
+                            className="mySwiper"
+                        >
+                            {props.popular.map((popular, index) =>
+                                index < 10 ? (
+                                    <SwiperSlide key={index}>
+                                        <MoviePopular key={index} movie={popular} index={index} />
+                                    </SwiperSlide>
+                                ) : null
+                            )}
+                        </Swiper>
                     </ul>
                 </div>
             </div>
